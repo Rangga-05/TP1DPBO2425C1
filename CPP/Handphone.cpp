5024e4fc36daa43772588d54a1db63a8ef0a0135
Rangga <ranggaoke92@gmail.com>
2025-09-16 16:55:13 +0700

    Add files via upload
    Commit Kedua CPP

--- CPP/Handphone.cpp
+++ CPP/Handphone.cpp
@@ -12,7 +12,13 @@ class Handphone{
     string spesifikasi;
 
     public:
-    Handphone(){}
+    Handphone(){
+        this->id = 0;
+        this->merek = "";
+        this->jenis = "";
+        this->harga = "";
+        this->spesifikasi = "";
+    }
 
     Handphone(int id, string merek, string jenis, string harga, string spesifikasi){
         this->id = id;
@@ -23,6 +29,9 @@ class Handphone{
     }
 
     //Set Get
+    void setID(int id){
+        this->id = id;
+    }
     int getId(){
         return id;
     }
@@ -55,125 +64,6 @@ class Handphone{
         return spesifikasi;
     }
 
-    void ingfoHandphone(){
-        cout << "ID: " << id
-             << " | Merek: " << merek
-             << " | Jenis: " << jenis
-             << " | Harga: Rp" << harga
-             << " | Spesifikasi: " << spesifikasi << endl;
-    }
-
     //Destruktor
     ~Handphone(){}
 };
-
-/*CRUD*/
-Handphone daftar[100];
-int jumlahData = 0;
-
-int aturId(){
-    int i;
-    for(i = 1; i <= jumlahData + 1; i++){
-        int ketemu = 0;
-        for(int k = 0; k < jumlahData; k++){
-            if(daftar[k].getId() == i){
-                ketemu++;
-            }
-        }
-        if(ketemu == 0){
-            return i;
-        }
-    }
-    return jumlahData + 1;
-}
-
-void tambahData(){
-    int id = aturId();
-    string merek, jenis, harga, spesifikasi;
-
-    cout << "Merek: "; getline(cin, merek);
-    cout << "Jenis: "; getline(cin, jenis);
-    cout << "Harga: "; getline(cin, harga);
-    cout << "Spesifikasi: "; getline(cin, spesifikasi);
-
-    daftar[jumlahData] = Handphone(id, merek, jenis, harga, spesifikasi);
-    jumlahData++;
-    cout << "Data Ditambahkan\n";
-}
-
-void tampilkanData(){
-    if(jumlahData == 0){
-        cout << "Data Belum Ada\n";
-        return;
-    }
-
-    for(int i = 0; i < jumlahData - 1; i++){
-        for(int j = 0; j < jumlahData - i - 1; j++){
-            if(daftar[j].getId() > daftar[j + 1].getId()){
-                Handphone temp = daftar[j];
-                daftar[j] = daftar[j + 1];
-                daftar[j + 1] = temp;
-            }
-        }
-    }
-
-    cout << "\n___ Daftar Handphone ___\n";
-    for(int i = 0; i < jumlahData; i++){
-        daftar[i].ingfoHandphone();
-    }
-}
-
-void updateData(){
-    int id;
-    cout << "ID Handphone Yang Ingin Diupdate: "; cin >> id; cin.ignore();
-
-    for(int i = 0; i < jumlahData; i++){
-        if(daftar[i].getId() == id){
-            string merek, jenis, harga, spesifikasi;
-            cout << "Update Merek: "; getline(cin, merek);
-            cout << "Update Jenis: "; getline(cin, jenis);
-            cout << "Update Harga: "; getline(cin, harga);
-            cout << "Update Spesifikasi: "; getline(cin, spesifikasi);
-
-            daftar[i].setMerek(merek);
-            daftar[i].setJenis(jenis);
-            daftar[i].setHarga(harga);
-            daftar[i].setSpesifikasi(spesifikasi);
-
-            cout << "Data Diupdate\n";
-            return;
-        }
-    }
-    cout << "ID " << id << " Tidak Ditemukan\n";
-}
-
-void hapusData(){
-    int id;
-    cout << "ID Handphone Yang Ingin Dihapus: "; cin >> id; cin.ignore();
-
-    for(int i = 0; i < jumlahData; i++){
-        if(daftar[i].getId() == id){
-            for(int j = i; j < jumlahData - 1; j++){
-                daftar[j] = daftar[j + 1];
-            }
-            jumlahData--;
-            cout << "Data Dihapus\n";
-            return;
-        }
-    }
-    cout << "ID " << id << " Tidak Ditemukan\n";
-}
-
-void cariData(){
-    int id;
-    cout << "ID Yang Ingin Dicari: "; cin >> id; cin.ignore();
-
-    for(int i = 0; i < jumlahData; i++){
-        if(daftar[i].getId() == id){
-            cout << "Data Ditemukan:\n";
-            daftar[i].ingfoHandphone();
-            return;
-        }
-    }
-    cout << "ID " << id << " Tidak Ditemukan\n";
-}
--- CPP/Main.cpp
+++ CPP/Main.cpp
@@ -1,6 +1,131 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "Handphone.cpp"
 using namespace std;
+
+vector<Handphone> listHandphone;
+
+void tambahData(){
+    int id;
+    string merek, jenis, harga, spesifikasi;
+
+    cout << "ID: "; cin >> id; cin.ignore();
+
+    int i;
+    for(i = 0; i < listHandphone.size(); i++){
+        if(listHandphone[i].getId() == id){
+            cout << "ID Sudah Dipakai\n";
+            return;
+        }
+    }
+
+    cout << "Merek: "; getline(cin, merek);
+    cout << "Jenis: "; getline(cin, jenis);
+    cout << "Harga: "; getline(cin, harga);
+    cout << "Spesifikasi: "; getline(cin, spesifikasi);
+
+    Handphone Hp(id, merek, jenis, harga, spesifikasi);
+    listHandphone.push_back(Hp);
+    cout << "Data Ditambahkan\n";
+}
+
+void tampilkanData(){
+    cout << "\n___ Daftar Handphone Rangga Store___\n";
+    if(listHandphone.size() == 0){
+        cout << "Data Belum Ada\n";
+    }
+    else{
+        for(int i = 0; i < listHandphone.size(); i++){
+            cout << "ID: " << listHandphone[i].getId()
+                 << " | Merek: " << listHandphone[i].getMerek()
+                 << " | Jenis: " << listHandphone[i].getJenis()
+                 << " | Harga: " << listHandphone[i].getHarga()
+                 << " | Spesifikasi: " << listHandphone[i].getSpesifikasi() << endl;
+        }
+    }
+}
+
+void updateData(){
+    if(listHandphone.size() == 0){
+        cout << "Data Belum Ada\n";
+        return;
+    }
+
+    int id;
+    cout << "ID Handphone Yang Ingin Diupdate: "; cin >> id; cin.ignore();
+
+    int ketemu = 0;
+    for(int i = 0; i < listHandphone.size(); i++){
+        if(listHandphone[i].getId() == id){
+            string merek, jenis, harga, spesifikasi;
+            cout << "Update Merek: "; getline(cin, merek);
+            cout << "Update Jenis: "; getline(cin, jenis);
+            cout << "Update Harga: "; getline(cin, harga);
+            cout << "Update Spesifikasi: "; getline(cin, spesifikasi);
+
+            listHandphone[i].setMerek(merek);
+            listHandphone[i].setJenis(jenis);
+            listHandphone[i].setHarga(harga);
+            listHandphone[i].setSpesifikasi(spesifikasi);
+
+            ketemu++;
+        }
+    }
+    if(ketemu > 0){
+        cout << "Data Diupdate\n";
+    }
+    else{
+        cout << "ID Tidak Ditemukan\n";
+    }
+}
+
+void hapusData(){
+    if(listHandphone.size() == 0){
+        cout << "Data Belum Ada\n";
+        return;
+    }
+
+    int id;
+    cout << "ID Handphone Yang Ingin Dihapus: "; cin >> id;
+
+    int hitung = listHandphone.size();
+    for(int i = 0; i < listHandphone.size(); i++){
+        if(listHandphone[i].getId() == id){
+            listHandphone.erase(listHandphone.begin() + i);
+            i--;
+        }
+    }
+    if(listHandphone.size() < hitung){
+        cout << "Data Dihapus\n";
+    }
+    else{
+        cout << "ID Tidak Ditemukan\n";
+    }
+}
+
+void cariData(){
+    if(listHandphone.size() == 0){
+        cout << "Data Belum Ada\n";
+        return;
+    }
+
+    int id;
+    cout << "ID Yang Ingin Dicari: "; cin >> id; cin.ignore();
+
+    int ketemu = 0;
+    for(int i = 0; i < listHandphone.size(); i++){
+        if(listHandphone[i].getId() == id){
+            cout << "ID: " << listHandphone[i].getId()
+                 << " | Merek: " << listHandphone[i].getMerek()
+                 << " | Jenis: " << listHandphone[i].getJenis()
+                 << " | Harga: " << listHandphone[i].getHarga()
+                 << " | Spesifikasi: " << listHandphone[i].getSpesifikasi() << endl;
+            ketemu++;
+        }
+    }
+    if(ketemu == 0){
+        cout << "ID Tidak Ditemukan\n";
+    }
+}
 
 int main(){
     int menu;
